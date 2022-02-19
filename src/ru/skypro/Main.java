8948ac78ae39bc10e9d6d49138e40f2cf89fb88b
Namig1991 <safarovnamig09@gmail.com>
2022-02-19 14:47:19 +0300

    My commit number2.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("ФИО Сотрудника - " + fullName);
+        System.out.println();
+
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.print("Данные ФИО сотрудника для заполнения отчета — " + fullNameUpper);
+        System.out.println();
+
+        String fullName1 = fullName.replace("Ivanov Ivan Ivanovich", "Иванов Семён Семёнович");
+        fullName1 = fullName1.replace("ё", "е");
+        fullName1 = fullName1.replace("Ё", "Е");
+        System.out.println("Данные ФИО сотрудника — " + fullName1);
     }
 }
